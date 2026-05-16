caceb2516c4abe4dce1f9a0f1a8bccbf6efccc11
Lemon0610 <92876763+Lemon0610@users.noreply.github.com>
2026-05-17 04:28:30 +0900

    feat: 스플래시 화면 로고 추가 및 테마 설정 수정

--- app/src/main/java/com/cmc/caudex/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/cmc/caudex/presentation/splash/SplashScreen.kt
@@ -1,21 +1,23 @@
 package com.cmc.caudex.presentation.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.sp
+import com.cmc.caudex.R
 import com.cmc.caudex.presentation.designsystem.theme.CaudexTheme
 import kotlinx.coroutines.delay
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SplashScreen(onSplashComplete: () -> Unit) {
@@ -27,18 +29,16 @@ fun SplashScreen(onSplashComplete: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(CaudexTheme.colors.k50),
+            .background(CaudexTheme.colors.k5),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-
-        // TODO: 앱 이름 또는 로고 텍스트로 교체
-        Text(
-            text = "Caudex",
-            color = CaudexTheme.colors.primary,
-            fontSize = 28.sp,
-            fontWeight = FontWeight.SemiBold,
-            modifier = Modifier.offset { IntOffset(x = 0, y = -300) },
+        Image(
+            painter = painterResource(id = R.drawable.oddnary),
+            contentDescription = "ODDNARY LOGO",
+            modifier = Modifier
+                .size(width = 180.dp, height = 40.dp)
+                .offset { IntOffset(x = 0, y = -300) }
         )
     }
 }
