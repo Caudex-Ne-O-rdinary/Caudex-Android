31fb7f06876ac4f268b26a67f3c4ceef773e1ff3
Lemon0610 <92876763+Lemon0610@users.noreply.github.com>
2026-05-16 22:35:57 +0900

    refactor: GreenButton을 CaudexButton으로 변경 및 Preview 추가

--- app/src/main/java/com/cmc/caudex/presentation/designsystem/components/CaudexButton.kt
+++ app/src/main/java/com/cmc/caudex/presentation/designsystem/components/CaudexButton.kt
@@ -8,18 +8,18 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.cmc.caudex.presentation.designsystem.theme.LocalCaudexColors
+import com.cmc.caudex.presentation.designsystem.theme.CaudexTheme
 import com.cmc.caudex.presentation.designsystem.theme.LocalCaudexTypography
 
 @Composable
-fun GreenButton(
+fun CaudexButton(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true
 ) {
-    val colors = LocalCaudexColors.current
     Button(
         onClick = onClick,
         modifier = modifier
@@ -28,15 +28,38 @@ fun GreenButton(
         enabled = enabled,
         shape = RoundedCornerShape(8.dp),
         colors = ButtonDefaults.buttonColors(
-            containerColor = colors.primary,
-            contentColor = colors.k5,
-            disabledContainerColor = colors.k400,
-            disabledContentColor = colors.k5
+            containerColor = CaudexTheme.colors.primary,
+            contentColor = CaudexTheme.colors.k5,
+            disabledContainerColor = CaudexTheme.colors.k400,
+            disabledContentColor = CaudexTheme.colors.k5
         )
     ) {
         Text(
             text = text,
             style = LocalCaudexTypography.current.title2
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CaudexButtonEnablePreview() {
+    CaudexTheme {
+        CaudexButton(
+            text = "확인",
+            onClick = {},
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CaudexButtonDisablePreview() {
+    CaudexTheme {
+        CaudexButton(
+            text = "확인",
+            onClick = {},
+            enabled = false
         )
     }
 }
